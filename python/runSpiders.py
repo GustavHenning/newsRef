bdd7f9a2e130bf4be25460e4614562b7aba6ae3d
Gustav <nightmunnas@gmail.com>
2017-02-19 21:46:53 +0100

    removed exp aft for pressing ceremonial reasons

--- python/runSpiders.py
+++ python/runSpiders.py
@@ -1,12 +1,13 @@
 import spider, sys
 
+
+
 def main(dryrun):
     print dryrun
     # source, baseurl, regex on valid article links
     spider.main('http://www.dn.se','http://www.dn.se', '^/(nyheter|debatt|ekonomi|insidan|sport|sthlm)/(.+-.+-.+)/$', dryrun) # three layers of article words
     spider.main('http://www.svd.se','http://www.svd.se', '^/(.+-.+)/', dryrun) # the first words are connected by hyphens
     spider.main('http://www.svt.se/nyheter', 'http://www.svt.se', '^(/\w+)+(.+-.+)', dryrun) #one or more categories and then hyphen sentences
-    spider.main('http://www.expressen.se', 'http://www.expressen.se', '^(/\w+)+(.+-.+)+', dryrun)
-    spider.main('http://www.aftonbladet.se', 'http://www.aftonbladet.se', '^(/\w+)+', dryrun)
+
 if __name__=='__main__':
     sys.exit(main(sys.argv[1]))
